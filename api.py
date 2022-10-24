429f886021db5f54cbe6f59f3165dd1aa8f22081
wenlaihan <69778334+wenlaihan@users.noreply.github.com>
2022-10-24 17:09:59 -0500

    Update api.py
    Core functions updated get_connection() and executeDBQuery()

---
+++ api.py
@@ -0,0 +1,28 @@
+import sys
+import psycopg2
+import config
+
+
+def get_connection():
+    ''' Returns a database connection object with which you can create cursors,
+        issue SQL queries, etc. This function is extremely aggressive about
+        failed connections--it just prints an error message and kills the whole
+        program. Sometimes that's the right choice, but it depends on your
+        error-handling needs. '''
+    try:
+        return psycopg2.connect(database=config.database,
+                                user=config.user,
+                                password=config.password)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        exit()
+
+
+def executeDBQuery(connection, query):
+    try:
+        cursor = connection.cursor()
+        # Execute the query
+        cursor.execute(query)
+        return cursor
+    except Exception as e:
+        print(e, file=sys.stderr)
