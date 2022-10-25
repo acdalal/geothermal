9da25055b733f83c84ee48eb435933f9cd2c181b
Alia <alia.babinet@gmail.com>
2022-10-24 21:09:41 -0500

    added in temp vs. time function

--- api.py
+++ api.py
@@ -57,7 +57,7 @@ def executeDBQuery(connection: psycopg2.connection, query: str) -> list[tuple]:
 
 def findOutagesInUserData(startTime: str, endTime: str) -> list[tuple]:
     '''
-    Finds all data outages or errors in the given time range. 
+    Finds all data outages or errors in the given time range.
 
     Parameters
     ------------
@@ -71,8 +71,8 @@ def findOutagesInUserData(startTime: str, endTime: str) -> list[tuple]:
 
     '''
 
-    query = f''' SELECT outage_type_id, start_datetime_utc, end_datetime_utc, outage_type 
-                 FROM measurement_outage, outage_types 
+    query = f''' SELECT outage_type_id, start_datetime_utc, end_datetime_utc, outage_type
+                 FROM measurement_outage, outage_types
                  WHERE (start_datetime_utc BETWEEN {startTime} AND {endTime}) OR (end_datetime_utc BETWEEN {startTime} AND {endTime}) OR ({startTime} >= start_datetime_utc AND {endTime} <= end_datetime_utc'''
     try:
         connection = get_connection()
@@ -80,3 +80,52 @@ def findOutagesInUserData(startTime: str, endTime: str) -> list[tuple]:
         return allOutages
     except Exception as e:
         print(e, file=sys.stderr)
+
+def createTempVsTimeQuery(channel: int, depth: str):
+
+    '''
+    Returns the results of the query for temp vs time
+
+    Parameters
+
+    -----------
+    channel
+    depth
+
+    Returns
+
+    -----------
+    JSON formatted temperature vs time data for constant depth
+
+
+    '''
+
+    query = '''SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
+                    dts_data.temperature_c, dts_data.datetime_utc
+                FROM measurement, dts_data
+                WHERE measurement.channel_id IN (SELECT id FROM channel
+                                                WHERE channel_name = 'chanel %s')
+                AND measurement.depth_m
+                AND measurement.id = dts_data.measurement_id'''
+
+    results = []
+
+
+    try:
+        connection = getConnection()
+        cursor = connection.cursor()
+        cursor.execute(query, (channel, depth))
+
+        for row in cursor:
+            datapoint = {'channel_id':row[0],
+                        'measurement_id':row[1],
+                        'datetime_utc':row[2].strftime(f"%Y-%,-%d %H:%M:%S"),
+                        'data_id':row[3],
+                        'temperature_c':row[4],
+                        'datetime'}
+
+            results.append(datapoint)
+    except Exception as e:
+        print(e, file = sys.stderr)
+
+    return json.dumps(results)
