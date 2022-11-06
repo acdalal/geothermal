4fa7aec114ad88d08d69e46708f8cfed099fc29b
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2022-11-06 12:47:51 -0600

    Restructed the code, separated views and api

--- geothermalsite/dashboard/api.py
+++ geothermalsite/dashboard/api.py
@@ -1,64 +1,195 @@
 import sys
+from django.http import HttpResponse
+from django.db import connections
+from datetime import datetime, timedelta
 import psycopg2
-import config
+from . import config
 import json
 from datetime import datetime, timedelta
 import dateparser as dp
 
 
-def getConnection() -> psycopg2.connection:
-
+def _createDataOutageQuery(startTime: str, endTime: str) -> str:
     """
-    Returns a database connection object with which you can create cursors,
-    issue SQL queries, etc.
+    Creates a query for retrieving data outages during the selected period of time
 
     Parameters
     -----------
-    None
+    startTime: start of the time range
+    endTime: end of the time range
 
     Returns
-    ------------
-    A psycopg2 cursor object
+    -----------
+    Formatted query to be executed by the database cursor
+
     """
 
-    try:
-        return psycopg2.connect(
-            database=config.DATABASE, user=config.USER, password=config.PASSWORD
-        )
-    except Exception as e:
-        print(e, file=sys.stderr)
-        exit()
+    query = f""" SELECT outage_type_id, start_datetime_utc, end_datetime_utc, outage_type
+                 FROM measurement_outage, outage_types
+                 WHERE (start_datetime_utc BETWEEN {startTime} AND {endTime}) OR
+                 (end_datetime_utc BETWEEN {startTime} AND {endTime}) OR
+                 ({startTime} >= start_datetime_utc AND {endTime} <= end_datetime_utc)"""
+
+    return query
 
 
-def executeDBQuery(connection: psycopg2.connection, query: str) -> list[tuple]:
+def _createTempVsTimeQuery(
+    channel: int, depth: str, startTime: str, endTime: str
+) -> str:
     """
-    Executes the passed query on the database and throws an exception if it encounters an error.
+    Creates a query for getting temperature vs time results for fixed depth
 
     Parameters
-    -------------
-    connection: A psycongp2 connection object for the database
-
-    query: The query to be excuted
-
+    -----------
+    channel: ID of the borehole to query
+    depth: depth of the measurements held constant
+    startTime: start of the time range
+    endTime: end of the time range
 
     Returns
-    ------------
-    The output of the query
+    -----------
+    Formatted query to be executed by the database cursor
+
 
     """
 
-    try:
-        cursor = connection.cursor()
-        # Execute the query
+    query = f"""SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
+            dts_data.temperature_c, dts_data.depth_m
+            FROM measurement
+            JOIN dts_data
+            ON measurement.id = dts_data.measurement_id
+            WHERE measurement.channel_id IN (SELECT id FROM channel WHERE
+                                             channel_name={channel})
+            AND dt_data.depth_m = {depth}
+            limit 5;
+            """
+
+    return query
+
+
+def _createTempVsDepthQuery(channel: int, timestamp: str) -> str:
+
+    """
+    Creates a query for getting temperature vs depth results for fixed depth
+
+    Parameters
+    -----------
+    channel: ID of the borehole to query
+    timestamp: point in time for the query; if no measurements taken at that exact moment, the closest measurements are returned instead
+
+    Returns
+    -----------
+    Formatted query to be executed by the database cursor
+
+
+    """
+    startTime = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+    endTime = startTime + timedelta(hours=1)
+
+    query = f"""SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
+            dts_data.temperature_c, dts_data.depth_m
+            FROM measurement, dts_data
+            WHERE measurement.channel_id IN (SELECT id FROM channel WHERE
+                                             channel_name='channel {channel}')
+            AND measurement.datetime_utc between {startTime} AND {endTime}
+            AND measurement.id = dts_data.measurement_id
+            """
+
+    return query
+
+
+def _countMeasurement(request) -> list[tuple]:
+    """
+    Proof of concept query
+    """
+    query = "select COUNT(*) from measurement"
+    with connections["geothermal"].cursor() as cursor:
         cursor.execute(query)
-        queryResult = cursor.fetchall()
-        cursor.close()
-        return queryResult
-    except Exception as e:
-        print(e, file=sys.stderr)
+        results = cursor.fetchall()
+    return results
 
 
-def findOutagesInUserData(startTime: str, endTime: str) -> list[tuple]:
+def getTempVsDepthResults(channel: int, timestamp: str) -> list[dict]:
+    """
+    Returns a list of all data points across all measurements associated with
+    the channel during that hour.
+
+    Parameters
+    ----------
+    channel: ID of the borehole (1 or 3)
+    timestamp: start time of the query
+
+    Returns
+    ----------
+    A dictionary with the query data (column label:data point)
+
+    Example
+    ----------
+    TODO
+    """
+
+    query = _createTempVsDepthQuery(channel, timestamp)
+    results = list()
+
+    with connections["geothermal"].cursor() as cursor:
+        cursor.execute(query)
+
+        for row in cursor.fetchall():
+            datapoint = {
+                "channel_id": row[0],
+                "measurement_id": row[1],
+                "datatime_utc": row[2].strftime(f"%Y-%m-%d %H:%M:%S"),
+                "data_id": row[3],
+                "temperature_c": row[4],
+                "depth_m": row[5],
+            }
+            results.append(datapoint)
+
+    return results
+
+
+def getTempVsTimeResults(
+    channel: int, depth: str, startTime: str, endTime: str
+) -> list[dict]:
+    """
+    Returns a list of all data points across all measurements associated with
+    the channel and depth for a given time range.
+
+    Parameters
+    ----------
+    channel: ID of the borehole (1 or 3)
+    depth: depth of temperature measurement
+    startTime: start time of the query
+    endTime: end time of the query
+
+    Returns
+    ----------
+    A dictionary with the results of the query
+
+    Example
+    ----------
+    TODO
+    """
+    query = _createTempVsTimeQuery(channel, depth, startTime, endTime)
+    results = list()
+
+    with connections["geothermal"].cursor() as cursor:
+        cursor.execute(query)
+        for row in cursor.fetchall():
+            datapoint = {
+                "channel_id": row[0],
+                "measurement_id": row[1],
+                "datatime_utc": row[2].strftime(f"%Y-%m-%d %H:%M:%S"),
+                "data_id": row[3],
+                "temperature_c": row[4],
+                "depth_m": row[5],
+            }
+            results.append(datapoint)
+
+    return results
+
+
+def getDataOutages(startTime: str, endTime: str) -> list[tuple]:
     """
     Finds all data outages or errors in the given time range.
 
@@ -74,106 +205,17 @@ def findOutagesInUserData(startTime: str, endTime: str) -> list[tuple]:
 
     """
 
-    query = f""" SELECT outage_type_id, start_datetime_utc, end_datetime_utc, outage_type
-                 FROM measurement_outage, outage_types
-                 WHERE (start_datetime_utc BETWEEN {startTime} AND {endTime}) OR (end_datetime_utc BETWEEN {startTime} AND {endTime}) OR ({startTime} >= start_datetime_utc AND {endTime} <= end_datetime_utc"""
-    try:
-        connection = getConnection()
-        allOutages = executeDBQuery(connection, query)
-        return allOutages
-    except Exception as e:
-        print(e, file=sys.stderr)
+    query = _createDataOutageQuery(startTime, endTime)
+    results = list()
 
-
-def createTempVsTimeQuery(channel: int, depth: str) -> dict:
-
-    """
-    Returns the results of the query for temp vs time
-
-    Parameters
-    -----------
-    channel
-    depth
-
-    Returns
-    -----------
-    JSON formatted temperature vs time data for constant depth
-
-
-    """
-
-    query = """SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
-                    dts_data.temperature_c, dts_data.datetime_utc
-                FROM measurement, dts_data
-                WHERE measurement.channel_id IN (SELECT id FROM channel
-                                                WHERE channel_name = 'chanel %s')
-                AND measurement.depth_m
-                AND measurement.id = dts_data.measurement_id"""
-
-    results = []
-
-    try:
-        connection = getConnection()
-        cursor = connection.cursor()
-        cursor.execute(query, (channel, depth))
-
-        for row in cursor:
+    with connections["geothermal"].cursor() as cursor:
+        cursor.execute(query)
+        for row in cursor.fetchall():
             datapoint = {
-                "channel_id": row[0],
-                "measurement_id": row[1],
-                "datetime_utc": row[2].strftime(f"%Y-%,-%d %H:%M:%S"),
-                "data_id": row[3],
-                "temperature_c": row[4],
-                "datetime": row[5],
-            }
-
-            results.append(datapoint)
-    except Exception as e:
-        print(e, file=sys.stderr)
-
-
-def createTempVsDepthQuery(channel: int, startHour: str):
-    """Given a channel (1 or 3) and a startHour (in string datetime
-    format: '%Y-%m-%d %H:%M:%S'), returns a list of all data points
-    across all measurements associated with a the channel during that hour
-    in JSON format.
-    """
-
-    query = """SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
-            dts_data.temperature_c, dts_data.depth_m
-            FROM measurement, dts_data
-            WHERE measurement.channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name='channel %s')
-            AND measurement.datetime_utc between %s AND %s
-            AND measurement.id = dts_data.measurement_id
-            """
-    results = []
-    startHourDatetime = datetime.strptime(startHour, f"%Y-%m-%d %H:%M:%S")
-    endHourDatetime = startHourDatetime + timedelta(hours=1)
-    endHour = endHourDatetime.strftime(f"%Y-%m-%d %H:%M:%S")
-
-    try:
-        connection = getConnection()
-        cursor = connection.cursor()
-        cursor.execute(
-            query,
-            (
-                channel,
-                startHour,
-                endHour,
-            ),
-        )
-        for row in cursor:
-            datapoint = {
-                "chanel_id": row[0],
-                "measurement_id": row[1],
-                "datatime_utc": row[2].strftime(f"%Y-%m-%d %H:%M:%S"),
-                "data_id": row[3],
-                "temperature_c": row[4],
-                "depth_m": row[5],
+                "outage_id": row[0],
+                "start_time": row[1],
+                "end_time": row[2],
+                "outage_type": row[3],
             }
             results.append(datapoint)
-    except Exception as e:
-        print(e, file=sys.stderr)
-
-    return json.dumps(results)
+    return results
--- geothermalsite/dashboard/views.py
+++ geothermalsite/dashboard/views.py
@@ -1,161 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.db import connections
-from datetime import datetime, timedelta
 import dateparser
 
 from .forms import TempVsTimeForm
+from .api import getTempVsDepthResults, getTempVsTimeResults, getDataOutages
 
 
 def index(request):
     if request.method == "POST":
+
         userForm = TempVsTimeForm(request.POST)
-        # check whether it's valid:
         if userForm.is_valid():
             channelNumber = userForm.cleaned_data["channelNumber"]
             startDate = userForm.cleaned_data["startDate"]
             endDate = userForm.cleaned_data["endDate"]
 
             startDateUtc = dateparser.parse(startDate).__str__()
-
             endDateUtc = dateparser.parse(endDate).__str__()
-            print(channelNumber, startDateUtc, endDateUtc)
+
+            queryResults = getTempVsDepthResults(channelNumber, startDate)
+            return render(request, "dashboard/index.html", {"queryData": queryResults})
         else:
             print(userForm.errors)
     form = TempVsTimeForm()
     return render(request, "dashboard/index.html", {"form": form})
-
-
-def countMeasurement(request):
-    # proof of concept query
-
-    query = "select COUNT(*) from measurement"
-    with connections["geothermal"].cursor() as cursor:
-        cursor.execute(query)
-        results = cursor.fetchall()
-    return HttpResponse(results)
-
-
-def getTempVsDepthResults(request):
-    """
-    Returns a list of all data points across all measurements associated with
-    the channel during that hour.
-
-    Parameters
-    ----------
-    channel (int:1 or 3)
-    startHour (string datetime format: '%Y-%m-%d %H:%M:%S')
-
-    Returns
-    ----------
-    An HttpResponse holding a dictionary with form (column label:data point)
-
-    Example
-    ----------
-    URL: /dashboard/tempvsdepth?channel=1&startHour=2022-05-17 00:00:00
-    Result: all data points from channel 1 on 2022-05-17 from 00:00:00 to
-                                                              01:00:00
-    """
-
-    query = """SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
-            dts_data.temperature_c, dts_data.depth_m
-            FROM measurement
-            JOIN dts_data
-            ON measurement.id = dts_data.measurement_id
-            WHERE measurement.channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name=%s)
-            AND measurement.datetime_utc between %s AND %s;
-            """
-    results = []
-
-    channel = request.GET.get("channel")
-    channelName = "channel " + channel
-    startHour = request.GET.get("startHour")
-    startHourDatetime = datetime.strptime(startHour, f"%Y-%m-%d %H:%M:%S")
-    endHourDatetime = startHourDatetime + timedelta(hours=1)
-    endHour = endHourDatetime.strftime(f"%Y-%m-%d %H:%M:%S")
-
-    with connections["geothermal"].cursor() as cursor:
-        cursor.execute(
-            query,
-            (
-                channelName,
-                startHour,
-                endHour,
-            ),
-        )
-        for row in cursor.fetchall():
-            datapoint = {
-                "channel_id": row[0],
-                "measurement_id": row[1],
-                "datatime_utc": row[2].strftime(f"%Y-%m-%d %H:%M:%S"),
-                "data_id": row[3],
-                "temperature_c": row[4],
-                "depth_m": row[5],
-            }
-            results.append(datapoint)
-
-    return HttpResponse(results)
-
-def getTempVsTimeResults(request):
-    """
-    Returns a list of all data points across all measurements associated with
-    the channel and depth for a given time range.
-
-    Parameters
-    ----------
-    channel (int:1 or 3)
-    depth (int:**TBD**)
-    time range (string: week, month, year)
-
-    Returns
-    ----------
-    An HttpResponse holding a dictionary with form (column label:data point)
-
-    Example
-    ----------
-    URL: /dashboard/tempvstime?channel=1&depth=2&
-    Result: all data points from channel 1 on 2022-05-17 from 00:00:00 to
-                                                              01:00:00
-    """
-    query = """SELECT channel_id, measurement_id, datetime_utc, dts_data.id,
-            dts_data.temperature_c, dts_data.depth_m
-            FROM measurement
-            JOIN dts_data
-            ON measurement.id = dts_data.measurement_id
-            WHERE measurement.channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name=%s)
-            AND dt_data.depth_m = %s
-            limit 5;
-            """
-    results = []
-
-    channel = request.GET.get("channel")
-    channelName = "channel " + channel
-    # TO DO:
-    # startHour = request.GET.get("startHour")
-    # startHourDatetime = datetime.strptime(startHour, f"%Y-%m-%d %H:%M:%S")
-    # endHourDatetime = startHourDatetime + timedelta(hours=1)
-    # endHour = endHourDatetime.strftime(f"%Y-%m-%d %H:%M:%S")
-
-    with connections["geothermal"].cursor() as cursor:
-        cursor.execute(
-            query,
-            (
-                channelName,
-                depth,
-                timeRange,
-            ),
-        )
-        for row in cursor.fetchall():
-            datapoint = {
-                "channel_id": row[0],
-                "measurement_id": row[1],
-                "datatime_utc": row[2].strftime(f"%Y-%m-%d %H:%M:%S"),
-                "data_id": row[3],
-                "temperature_c": row[4],
-                "depth_m": row[5],
-            }
-            results.append(datapoint)
-
-    return HttpResponse(results)
