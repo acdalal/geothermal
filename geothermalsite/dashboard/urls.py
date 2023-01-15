792f8f1b4045405512ae65a73dd1167e95e0dca6
littone39 <63129949+littone39@users.noreply.github.com>
2023-01-15 15:35:03 -0600

    minimal tempvsdepth download

--- geothermalsite/dashboard/urls.py
+++ geothermalsite/dashboard/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("tempvstime/", views.tempVsTime, name="tempvstime"),
     path("tempvsdepth/", views.tempVsDepth, name="tempvsdepth"),
     path("tempvstime/download", views.tempVsTimeDownload, name="tempvstimedownload"),
+    path("tempvsdepth/download", views.tempVsDepthDownload, name="tempvsdepthdownload"),
     path("about/", views.about, name="about"),
 ]
