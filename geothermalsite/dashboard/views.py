a26545f52d55277710113bd8d5f319545cc09ef1
Henrie Friesen <henriepf@gmail.com>
2023-01-16 20:16:16 -0600

    move views helper functions to new file

--- geothermalsite/dashboard/views.py
+++ geothermalsite/dashboard/views.py
@@ -1,10 +1,6 @@
-import time
 from django.shortcuts import render
-import dateparser
-import re
 import csv
 from django.http import HttpResponse, HttpResponseRedirect
-from datetime import datetime
 
 from .forms import (
     TempVsTimeForm,
@@ -16,72 +12,14 @@ from .api import getTempVsDepthResults, getTempVsTimeResults, getDataOutages
 
 from .constants import DATA_START_DATE, DATA_END_DATE
 
-
-def _getQuerySelectionData(cleanedData: dict) -> dict:
-    queryType = cleanedData["queryType"]
-    return {"queryType": queryType}
-
-
-def _getTempVsTimeFormData(cleanedData: dict) -> dict:
-    boreholeNumber = cleanedData["boreholeNumber"]
-    depth = cleanedData["depth"]
-
-    dateRange = cleanedData["dateRange"]
-    dateList = re.findall(r"../../....", dateRange)
-    startDate, endDate = dateList
-
-    startDateUtc = dateparser.parse(startDate).__str__()
-    endDateUtc = dateparser.parse(endDate).__str__()
-
-    return {
-        "boreholeNumber": boreholeNumber,
-        "depth": depth,
-        "startDateUtc": startDateUtc,
-        "endDateUtc": endDateUtc,
-    }
-
-
-def _getTempVsDepthFormData(cleanedData: dict) -> dict:
-    boreholeNumber = cleanedData["boreholeNumber"]
-
-    timestamp = cleanedData["timestamp"]
-    timestampUtc = dateparser.parse(timestamp).__str__()
-
-    return {"timestampUtc": timestampUtc, "boreholeNumber": boreholeNumber}
-
-
-def _convertToTempVsTimeGraphData(queryResults: list, borehole: int) -> list:
-    graphData = list()
-
-    for datapoint in queryResults:
-        temperature = float(datapoint["temperature_c"])
-        datetimeString = datapoint["datetime_utc"]
-        dateTime = datetime.strptime(datetimeString, "%Y-%m-%d %H:%M:%S")
-        jsTime = int(time.mktime(dateTime.timetuple()))
-        graphData.append([jsTime, temperature])
-
-    return [graphData]
-
-
-def _convertToTempVsDepthData(queryResults: list, borehole: int) -> list:
-    graphData = list()
-
-    for datapoint in queryResults:
-        temperature = float(datapoint["temperature_c"])
-        depth = int(datapoint["depth_m"])
-        graphData.append([depth, temperature])
-
-    return [graphData]
-
-
-def _truncateDateTime(dates):
-    truncatedDates = []
-    for i in range(len(dates)):
-        dateDict = dates[i]
-        start = dateDict.get("start_time").date()
-        end = dateDict.get("end_time").date()
-        truncatedDates.append({"startDate": start, "endDate": end})
-    return truncatedDates
+from .viewshelpers import (
+    _getQuerySelectionData,
+    _getTempVsTimeFormData,
+    _getTempVsDepthFormData,
+    _convertToTempVsTimeGraphData,
+    _convertToTempVsDepthData,
+    _truncateDateTime,
+)
 
 
 def index(request):
@@ -313,8 +251,3 @@ def tempVsDepthDownload(request):
         return render(
             request, "dashboard/tempvstime.html", context={"form": TempVsDepthForm()}
         )
-
-
-def _getQuerySelectionData(cleanedData: dict) -> dict:
-    queryType = cleanedData["queryType"]
-    return {"queryType": queryType}
