d43b381377c00664a4bf2839dc28c3912f65b8d0
wenlaihan <69778334+wenlaihan@users.noreply.github.com>
2023-01-20 12:12:58 -0600

    attempt on adding the loading wheel as feedback to the user when running query

---
+++ geothermalsite/dashboard/static/loadingWheel.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+    console.log("Please wait 1...");
+
+    $("#tempvstimeform").submit(function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        // Show the loading spinner when the request is being processed
+        $("#loading-wheel-box").show();
+        console.log("Please wait 2...");
+
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: data,
+            success: function (response) {
+                // Do something with the response
+                // ...
+
+                // Hide the loading spinner when the request is done
+                $("#loading-wheel-box").hide();
+            },
+            error: function (response) {
+                // Handle errors
+                // ...
+                // Hide the loading spinner when the request is done
+                $("#loading-wheel-box").hide();
+            }
+        });
+    });
+});
