d6691c07a2a2109651bfd09012d45596da707d80
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2023-01-26 16:18:02 -0600

    Separated query creating into another file

--- geothermalsite/dashboard/helper/api.py
+++ geothermalsite/dashboard/helper/api.py
@@ -1,156 +1,14 @@
 import time
 import sys
 from django.db import connections
-from datetime import datetime, timedelta
-from .boreholes import boreholes
+from datetime import datetime
 from .logging import log_query_as_INFO
-from .constants import HOURS, DAYS, WEEKS, MONTHS, YEARS, GROUPS
-
-
-def _createEntireDataOutageQuery() -> str:
-    """
-    Creates a query for retrieving all data outages
-
-    Returns
-    --------
-    Formatted query to be executed by the database cursor
-    """
-    query = f""" SELECT id, channel_id, outage_type, start_datetime_utc, end_datetime_utc
-                 FROM measurement_outage
-                 """
-
-    return query
-
-
-def _createTempVsTimeQuery(
-    borehole: str, depth: str, startTime: str, endTime: str
-) -> str:
-    """
-    Creates a query for getting temperature vs time results for fixed depth
-
-    Parameters
-    -----------
-    channel: ID of the borehole to query
-    depth: depth of the measurements held constant
-    startTime: start of the time range
-    endTime: end of the time range
-
-    Returns
-    -----------
-    Formatted query to be executed by the database cursor
-    """
-
-    currentBorehole = boreholes[borehole]
-
-    channel = currentBorehole.getChannel()
-    lafStart = currentBorehole.getStart()
-    lafBottom = currentBorehole.getBottom()
-
-    query = f"""SELECT channel_id, measurement_id, datetime_utc, D.id,
-            temperature_c, depth_m
-            FROM dts_data AS D
-            INNER JOIN measurement AS M
-            ON M.id = measurement_id
-            INNER JOIN channel AS H
-            ON channel_id = H.id
-            INNER JOIN dts_config AS C
-            ON dts_config_id = C.id
-            WHERE channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name='channel {channel}')
-            AND ABS(depth_m-{depth}) < step_increment_m/2
-            AND laf_m BETWEEN {lafStart} AND {lafBottom}
-            AND datetime_utc BETWEEN '{startTime}' AND '{endTime}'
-            ORDER BY datetime_utc;
-            """
-
-    return query
-
-
-def _createTempVsDepthQuery(borehole: str, timestamp: datetime) -> str:
-    """
-    Creates a query for getting temperature vs depth results for fixed depth
-
-    Parameters
-    -----------
-    channel: ID of the borehole to query
-    timestamp: point in time for the query; if no measurements taken at that
-        exact moment, the closest measurements are returned instead
-
-    Returns
-    -----------
-    Formatted query to be executed by the database cursor
-    """
-    timestampStart = timestamp
-    timestampEnd = timestampStart + timedelta(minutes=30)
-
-    currentBorehole = boreholes[borehole]
-
-    channel = currentBorehole.getChannel()
-    lafStart = currentBorehole.getStart()
-    lafEnd = currentBorehole.getBottom()
-
-    query = f"""SELECT channel_id, measurement_id, datetime_utc, D.id,
-            temperature_c, depth_m
-            FROM measurement AS M
-            INNER JOIN dts_data AS D
-            ON M.id = D.measurement_id
-            WHERE channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name='channel {channel}')
-            AND datetime_utc between '{timestampStart}' AND '{timestampEnd}'
-            AND laf_m BETWEEN {lafStart} AND {lafEnd}
-            ORDER BY depth_m;
-            """
-
-    return query
-
-
-def _createStratigraphyQuery(
-    borehole: str, startTime: str, endTime: str, dailyTimestamp: str
-) -> str:
-    """
-    Creates a query for getting temperature vs time and depth for a given borehole
-
-    Parameters
-    -----------
-    channel: ID of the borehole to query
-    startTime: start of the time range
-    endTime: end of the time range
-
-    Returns
-    -----------
-    Formatted query to be executed by the database cursor
-    TODO: retrieve the data each day only at a certain timestamp
-    -------------------
-
-    """
-
-    currentBorehole = boreholes[borehole]
-
-    channel = currentBorehole.getChannel()
-    lafStart = currentBorehole.getStart()
-    lafBottom = currentBorehole.getBottom()
-
-    timestampStart = dailyTimestamp
-    timestampEnd = timestampStart + timedelta(minutes=30)
-
-    query = f"""SELECT channel_id, measurement_id, datetime_utc, D.id,
-            temperature_c, depth_m
-            FROM dts_data AS D
-            INNER JOIN measurement AS M
-            ON M.id = measurement_id
-            INNER JOIN channel AS H
-            ON channel_id = H.id
-            INNER JOIN dts_config AS C
-            ON dts_config_id = C.id
-            WHERE channel_id IN (SELECT id FROM channel WHERE
-                                             channel_name='channel {channel}')
-            AND laf_m BETWEEN {lafStart} AND {lafBottom}
-            AND datetime_utc BETWEEN '{startTime}' AND '{endTime}'
-            AND CAST(datetime_utc AS TIME) BETWEEN '{timestampStart}' AND '{timestampEnd}'
-            ORDER BY depth_m, datetime_utc;
-            """
-
-    return query
+from .createQueries import (
+    createEntireDataOutageQuery,
+    createStratigraphyQueryByDay,
+    createTempVsDepthQuery,
+    createTempVsTimeQuery,
+)
 
 
 def getTempVsDepthResults(borehole: str, timestamp: datetime) -> list[dict]:
@@ -168,7 +26,7 @@ def getTempVsDepthResults(borehole: str, timestamp: datetime) -> list[dict]:
     A dictionary with the query data (column label:data point)
     """
 
-    query = _createTempVsDepthQuery(borehole, timestamp)
+    query = createTempVsDepthQuery(borehole, timestamp)
     results = list()
 
     with connections["geothermal"].cursor() as cursor:
@@ -220,7 +78,7 @@ def getTempVsTimeResults(
     A dictionary with the results of the query
     """
 
-    query = _createTempVsTimeQuery(borehole, depth, startTime, endTime)
+    query = createTempVsTimeQuery(borehole, depth, startTime, endTime)
     results = list()
 
     with connections["geothermal"].cursor() as cursor:
@@ -270,7 +128,7 @@ def getStratigraphyResults(
     ----------
     A dictionary with the results of the query
     """
-    query = _createStratigraphyQuery(borehole, startTime, endTime, dailyTimestamp)
+    query = createStratigraphyQueryByDay(borehole, startTime, endTime, dailyTimestamp)
     results = list()
 
     with connections["geothermal"].cursor() as cursor:
@@ -324,7 +182,7 @@ def getDataOutages() -> list[dict]:
     time range. If no outages happened, returns an empty list.
     """
 
-    query = _createEntireDataOutageQuery()
+    query = createEntireDataOutageQuery()
     results = list()
 
     with connections["geothermal"].cursor() as cursor:
