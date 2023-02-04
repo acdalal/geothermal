b92442b7c10793e2b956ddd7642ef3e26cf114aa
wenlaihan <69778334+wenlaihan@users.noreply.github.com>
2023-02-04 15:14:30 -0600

    incorporating stratigraphy form into index form, might just end up copy the entire stratigraphy to index

--- geothermalsite/dashboard/static/stratigraphyGraph.js
+++ geothermalsite/dashboard/static/stratigraphyGraph.js
@@ -1,58 +1,58 @@
 const $chart = document.getElementById('ctx')
 
 const drawVerticalLine = {
-    id: 'verticalLiner',
-    afterInit: (chart, args, opts) => {
-      chart.verticalLiner = {}
-    },
-    afterEvent: (chart, args, options) => {
-        const {inChartArea} = args
-        chart.verticalLiner = {draw: inChartArea}
-    },
-    beforeTooltipDraw: (chart, args, options) => {
-        const {draw} = chart.verticalLiner
-        if (!draw) return
+  id: 'verticalLiner',
+  afterInit: (chart, args, opts) => {
+    chart.verticalLiner = {}
+  },
+  afterEvent: (chart, args, options) => {
+    const { inChartArea } = args
+    chart.verticalLiner = { draw: inChartArea }
+  },
+  beforeTooltipDraw: (chart, args, options) => {
+    const { draw } = chart.verticalLiner
+    if (!draw) return
 
-        const {ctx} = chart
-        const {top, bottom} = chart.chartArea
-        const {tooltip} = args
-        const x = tooltip?.caretX
-        if (!x) return
+    const { ctx } = chart
+    const { top, bottom } = chart.chartArea
+    const { tooltip } = args
+    const x = tooltip?.caretX
+    if (!x) return
 
-        ctx.save()
+    ctx.save()
 
-        ctx.beginPath()
-        ctx.moveTo(x, top)
-        ctx.lineTo(x, bottom)
-        ctx.stroke()
+    ctx.beginPath()
+    ctx.moveTo(x, top)
+    ctx.lineTo(x, bottom)
+    ctx.stroke()
 
-        ctx.restore()
-    }
+    ctx.restore()
+  }
 }
 
 const fillChart = {
-    id: 'customCanvasBackgroundColor',
-    beforeDraw: (chart, args, options) => {
-      const {ctx} = chart;
-      ctx.save();
-      ctx.globalCompositeOperation = 'destination-over';
-      ctx.fillStyle = options.color || '#ffffff';
-      ctx.fillRect(0, 0, chart.width, chart.height);
-      ctx.restore();
-    }
-  };
+  id: 'customCanvasBackgroundColor',
+  beforeDraw: (chart, args, options) => {
+    const { ctx } = chart;
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = options.color || '#ffffff';
+    ctx.fillRect(0, 0, chart.width, chart.height);
+    ctx.restore();
+  }
+};
 
 const data = {
   datasets: [{
     data: graphData,
     label: "Temperature vs Depth Graph",
-}]
+  }]
 }
 
 var depth = "_depth_" + queryData[0]['depth_m'];
 var startDate = "_startDate_" + queryData[0]['datetime_utc'].slice(0, 11) // Cut off the timestamp
 var endDate = "_endDate" + queryData[queryData.length - 1]['datetime_utc'].slice(0, 11) // Cut off the timestamp
-const graphImageName = "geothermal_data"  + startDate + endDate + ".png";
+const graphImageName = "geothermal_data" + startDate + endDate + ".png";
 
 const options = {
   type: 'line',
@@ -61,14 +61,14 @@ const options = {
     indexAxis: 'y',
     label: "Temperature vs Depth Graph",
     legend: {
-        onClick: null
+      onClick: null
     },
     interaction: {
-        mode: 'index',
-        intersect: false,
+      mode: 'index',
+      intersect: false,
     },
     plugins: {
-        verticalLiner: {}
+      verticalLiner: {}
     },
     scales: {
       x: {
@@ -76,23 +76,36 @@ const options = {
       }
     },
     animation: {
-        onComplete: function(){
-            window.downloadGraphImage = function(){
-                var image = chart.toBase64Image()
-                const a = document.createElement('a')
-                a.href = image
-                a.download = graphImageName
-                document.body.appendChild(a)
-                a.click()
-                document.body.removeChild(a)
-            };
-        }
+      onComplete: function () {
+        window.downloadGraphImage = function () {
+          var image = chart.toBase64Image()
+          const a = document.createElement('a')
+          a.href = image
+          a.download = graphImageName
+          document.body.appendChild(a)
+          a.click()
+          document.body.removeChild(a)
+        };
+      }
     },
     pointRadius: 0,
     backgroundColor: 'rgba(255, 99, 132, 0.2)',
     borderColor: 'rgba(255, 99, 132, 1)',
     borderWidth: 1,
-},
-plugins: [drawVerticalLine, fillChart]
+  },
+  plugins: [drawVerticalLine, fillChart]
 }
 const chart = new Chart($chart, options)
+
+// $('html, body').animate({
+//   scrollTop: $("#stratigraphy-form").offset().top
+// }, 2000);
+
+$(document).ready(function () {
+  $("button").click(function () {
+    console.log("Submit clicked");
+    $(document).scrollTop($(document).height() * 0.5);
+  });
+});
+
+// document.getElementById("stratigraphy-form").scrollIntoView({ behavior: 'smooth' });
