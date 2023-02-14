52c397aea850c3f3725c8c82c5a572bf016e95d2
Kelly McGucken <mcguckenk@carleton.edu>
2023-02-14 10:05:14 -0600

    Changed name of site to GeoDE

--- geothermalsite/static/js/graphs/stratigraphyGraph.js
+++ geothermalsite/static/js/graphs/stratigraphyGraph.js
@@ -81,7 +81,7 @@ Object.keys(graphData).forEach(group => {
 
         datasets.push(lineData)
     })
-    if (groupNumber <= numGroups / 2){
+    if (groupNumber <= (numGroups / 2) - 1){
         green -= diff
         blue += diff
     }
@@ -116,7 +116,7 @@ const options = {
                 type: 'linear',
                 title: {
                     display: true,
-                    text: 'Depth below ground, m'
+                    text: 'Depth below ground, m.'
                 },
                 reverse: true
             }
