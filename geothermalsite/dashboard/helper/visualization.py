d82d14172e347bc9a37f21193cd12b2d5cabe932
viktorChekhovoi <viktor.chekhovoi@gmail.com>
2023-03-14 15:49:55 -0500

    Improved weeks labels

--- geothermalsite/dashboard/helper/visualization.py
+++ geothermalsite/dashboard/helper/visualization.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
 from .constants import (
     GROUPS,
@@ -29,25 +29,6 @@ def toChartJsTempVsTime(queryResults: list, units: str) -> list:
     return graphData
 
 
-# def toChartJsTempVsDepth(queryResults: list, units: str) -> list:
-#     """
-#     Takes the database query output from temperature vs depth query and modifies it to fit the format for the flot library
-#     """
-#     graphData = list()
-
-#     for datapoint in queryResults:
-#         if units == METRIC:
-#             temperature = float(datapoint["temperature_c"])
-#             depth = float(datapoint["depth_m"])
-#         else:
-#             temperature = float(datapoint["temperature_f"])
-#             depth = float(datapoint["depth_ft"])
-
-#         graphData.append({"x": depth, "y": temperature})
-
-#     return graphData
-
-
 def toChartJsTempProfile(
     queryResults: list, groupBy: int, units: str
 ) -> dict[dict[list[dict]]]:
@@ -68,7 +49,20 @@ def toChartJsTempProfile(
             if groupBy == DAYS:
                 group = f"{MONTH_SHORTHAND[date.month-1]} {date.day}, {date.year}"
             if groupBy == WEEKS:
-                group = f"Week {date.isocalendar()[1]}, {date.year}"
+                startDate = date - timedelta(days=date.weekday())
+                endDate = startDate + timedelta(days=7)
+                # the week falls within one month
+                if startDate.month == endDate.month:
+                    group = f"{MONTH_SHORTHAND[startDate.month]} {startDate.day} - {endDate.day}, {date.year}"
+
+                # different month, same year
+                elif startDate.year == endDate.year:
+                    group = f"{MONTH_SHORTHAND[startDate.month]} {startDate.day} - {MONTH_SHORTHAND[endDate.month]} {endDate.day}, {date.year}"
+
+                # different year
+                else:
+                    group = f"{MONTH_SHORTHAND[startDate.month]} {startDate.day}, {startDate.year} - {MONTH_SHORTHAND[endDate.month]} {endDate.day}, {endDate.year}"
+
             if groupBy == MONTHS:
                 group = f"{MONTH_SHORTHAND[date.month-1]}, {date.year}"
             if groupBy == YEARS:
